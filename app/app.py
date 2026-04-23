171dfb1bb60958dbdc985ffc9ce845077ff00b1d
Jaiden Jongjitirat <37761702+JaidenKemara@users.noreply.github.com>
2026-04-22 18:08:14 -0700

    Test to fix incorrect port number on dashboard card

--- app/app.py
+++ app/app.py
@@ -24,7 +24,9 @@ def index():
         "commit_sha": COMMIT_SHA[:7] if len(COMMIT_SHA) > 7 else COMMIT_SHA,
         "branch":     BRANCH,
         "workflow":   WORKFLOW,
-        "port":       "5000",
+        #"port":       "5000",
+        # TEST TO FIX INCORRECT PORT NUMBER
+        "port":       socket.getsockname()[1],
     }
     return render_template("index.html", **context)
 
